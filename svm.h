88cab5f15ef0b7f5b64923c5deac97dff9ee60eb
touahmed <touahmed@indiana.edu>
2016-03-24 23:22:42 -0400

    Removed overfeat

--- svm.h
+++ svm.h
@@ -193,11 +193,9 @@ protected:
             }
         }
 
-     //   cout<<"Feature Generation"<<endl;
         // Feature Generation
         for(int i=0;i<no_of_features;i++)
         {
-
             int which_feature = rand() % 5;
             int size_x = feature[which_feature][0];
             int size_y = feature[which_feature][1];
